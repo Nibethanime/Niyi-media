433711c51882e54c52c7c1a558c2572f976e38fe
Nibethanime <fortunatefelix35@gmail.com>
2026-07-16 02:37:43 +0000

    Delete app directory

--- app/Page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{background:'black', color:'white', minHeight:'100vh', padding:40, textAlign:'center'}}>
-      <h1 style={{fontSize:48}}>Nibeth AI</h1>
-      <p>Your site is live!</p>
-    </main>
-  )
-}
